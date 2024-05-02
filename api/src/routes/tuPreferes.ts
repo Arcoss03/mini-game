7ea3ecff398c6fe635f2c2cf6a1907aaca906b17
Arthur FRIN <frin.arthur@gmail.com>
2024-05-02 10:56:48 +0200

    feat: update tpf route in api

--- api/src/routes/tuPreferes.ts
+++ api/src/routes/tuPreferes.ts
@@ -1,11 +1,10 @@
 import { FastifyInstance, FastifyRequest, FastifyReply } from 'fastify';
-import db from '../db';
 
 interface RouteParams {
     id: string;
   }
 
-interface Post {
+interface TuPreferes {
   id?: number;
   prompt1: string;
   img_url1: string;
@@ -20,13 +19,13 @@ interface Post {
 
 
 async function routes(fastify: FastifyInstance) {
-    fastify.get('/posts', async (request, reply) => {
-      const [rows] = await fastify.db.query('SELECT * FROM posts');
+    fastify.get('/', async (request, reply) => {
+      const [rows] = await fastify.db.query('SELECT * FROM tu_preferes');
       return rows;
     });
 
-    fastify.get<{ Params: RouteParams }>('/posts/:id', async (request, reply) => {
-        const [rows]:any = await fastify.db.query('SELECT * FROM posts WHERE id = ?', [request.params.id]);
+    fastify.get<{ Params: RouteParams }>('/tpf/:id', async (request, reply) => {
+        const [rows]:any = await fastify.db.query('SELECT * FROM tu_preferes WHERE id = ?', [request.params.id]);
         if (rows.length === 0) {
           reply.status(404);
           return { error: 'Post not found' };
@@ -35,11 +34,11 @@ async function routes(fastify: FastifyInstance) {
         }
       });
 
-      fastify.post('/posts', async (request, reply) => {
+      fastify.post('/', async (request, reply) => {
         try {
-          const newPost:Post = request.body as Post;
+          const newPost:TuPreferes = request.body as TuPreferes;
           const result = await fastify.db.query(
-            'INSERT INTO posts (prompt1, img_url1, nb_clic1, prompt2, img_url2, nb_clic2, author_id, creation_date) VALUES (?, ?, ?, ?, ?, ?, ?, ?)', 
+            'INSERT INTO tu_preferes (prompt1, img_url1, nb_clic1, prompt2, img_url2, nb_clic2, author_id, creation_date) VALUES (?, ?, ?, ?, ?, ?, ?, ?)', 
             [newPost.prompt1, newPost.img_url1, newPost.nb_clic1, newPost.prompt2, newPost.img_url2, newPost.nb_clic2, newPost.author_id, new Date()]
           );
           reply.send({ result: "success" });
@@ -50,17 +49,17 @@ async function routes(fastify: FastifyInstance) {
       });
 
       //route tu update the number of clics
-      fastify.put<{ Params: RouteParams }>('/posts/vote/:id', async (request, reply) => {
-        const [rows]:any = await fastify.db.query('SELECT * FROM posts WHERE id = ?', [request.params.id]);
+      fastify.put<{ Params: RouteParams }>('/vote/:id', async (request, reply) => {
+        const [rows]:any = await fastify.db.query('SELECT * FROM tu_preferes WHERE id = ?', [request.params.id]);
         if (rows.length === 0) {
           reply.status(404);
           return { error: 'Post not found' };
         } else {
           const selectedClick = request.body as { selectedClick: number };
           if (selectedClick.selectedClick === 1) {
-            await fastify.db.query('UPDATE posts SET nb_clic1 = nb_clic1 + 1 WHERE id = ?', [request.params.id]);
+            await fastify.db.query('UPDATE tu_preferes SET nb_clic1 = nb_clic1 + 1 WHERE id = ?', [request.params.id]);
           } else if (selectedClick.selectedClick === 2) {
-            await fastify.db.query('UPDATE posts SET nb_clic2 = nb_clic2 + 1 WHERE id = ?', [request.params.id]);
+            await fastify.db.query('UPDATE tu_preferes SET nb_clic2 = nb_clic2 + 1 WHERE id = ?', [request.params.id]);
           } else {
             reply.status(400);
             return { error: 'Invalid click' };
