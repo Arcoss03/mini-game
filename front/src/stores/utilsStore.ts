e1f7f77a1cfc14a18dabc55aed548d47900a4c35
Arthur FRIN <frin.arthur@gmail.com>
2024-05-16 14:36:45 +0200

    feat: add utils Store with showToast function and apiHelper with get post put delete with token

---
+++ front/src/stores/utilsStore.ts
@@ -0,0 +1,23 @@
+import { ref } from 'vue'
+import { defineStore } from 'pinia'
+
+export const useUtilsStore = defineStore('utils', () => {
+  const toast = ref({
+    message: '',
+    isSuccess: false,
+    isVisible: false,
+});
+    
+
+
+const showToast = (error: string, isSuccess: boolean, ) => {
+    toast.value.isVisible = true;
+    toast.value.isSuccess = isSuccess;
+    toast.value.message = error;
+    setTimeout(() => {
+        toast.value.isVisible = false;
+    }, 3000);
+}
+
+return {toast, showToast}
+})
