2bd0d3493258e64fb07be79b8b4192315f882fd8
matteobelin <122531813+matteobelin@users.noreply.github.com>
2024-05-22 11:39:41 +0000

    fixe: ajout de addHook dans getProfil

--- api/src/routes/profil/getProfil.ts
+++ api/src/routes/profil/getProfil.ts
@@ -3,14 +3,22 @@ import type { JwtPayload } from '../../interfaces/JwtPayload';
 
 // Route to login a user with his token
 async function getProfilRoutes(fastify: FastifyInstance) {
-    fastify.get('/profil', async (request: FastifyRequest, reply: FastifyReply) => {
+    fastify.addHook('preHandler', async (request, reply) => {
         try {
             await request.jwtVerify();
-            const payload = request.user as JwtPayload;
-            if (!payload.id) {
-                reply.status(401).send({ error: 'Invalid token' });
-                return;
-            }
+        } catch (error) {
+            reply.status(401).send({ error: 'Invalid token' });
+        }
+    });
+    
+    fastify.get('/profil', async (request: FastifyRequest, reply: FastifyReply) => {
+        const payload = request.user as JwtPayload;
+        if (!payload.id) {
+            reply.status(401).send({ error: 'Invalid token' });
+            return;
+        }
+    
+        try {
             const [rows]: any = await fastify.db.query('SELECT id, pseudo, email, profil, creation_date FROM users WHERE id = ?', [payload.id]);
             if (rows.length === 0) {
                 reply.status(404).send({ error: 'User not found' });
@@ -18,7 +26,7 @@ async function getProfilRoutes(fastify: FastifyInstance) {
             }
             reply.send({ user: rows[0] });
         } catch (error) {
-            reply.status(401).send({ error: 'Invalid token' });
+            reply.status(500).send({ error: 'Database error' });
         }
     });
 }
