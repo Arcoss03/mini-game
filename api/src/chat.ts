3c45089f6fd8b381c12ce94076819a6afef53a32
matteobelin <matteobelin85@gmail.com>
2024-05-28 21:02:00 +0200

    feat: chat par room

--- api/src/chat.ts
+++ api/src/chat.ts
@@ -15,6 +15,18 @@ const setupSocket = (fastify: FastifyInstance) => {
   });
 
   io.on('connection', (socket) => {
+
+    socket.on('joinRoom', async function(room) {
+        const token = room.user;
+        const decoded = await fastify.jwt.verify(token);
+        const { id } = decoded as { id: string }; 
+        const [rows]: any = await fastify.db.query('SELECT pseudo FROM users JOIN chat_participant ON chat_participant.user_id = users.id WHERE users.id = ? AND chat_participant.chat_room_id = ?', [id,room.id]);
+        if(rows.length===1){
+          socket.join(room.id);
+        }
+  });
+
+
     socket.on('message', async (msg) => {
       try {
         const token = msg.pseudo;
@@ -23,7 +35,12 @@ const setupSocket = (fastify: FastifyInstance) => {
         const [rows]: any = await fastify.db.query('SELECT pseudo FROM users WHERE id = ?', [id]);
 
         if (rows.length >= 1) {
-          io.emit('messageResponse', { pseudo: rows[0].pseudo, content: msg.content });
+          const [rows]: any = await fastify.db.query('SELECT pseudo FROM users JOIN chat_participant ON chat_participant.user_id = users.id WHERE users.id = ? AND chat_participant.chat_room_id = ?', [id,msg.roomId]);
+          if(rows.length===1){
+            socket.to(msg.roomId).emit('messageResponse', { pseudo: rows[0].pseudo, content: msg.content,class:"red" });
+            socket.emit('messageResponse', { pseudo: rows[0].pseudo, content: msg.content,class:"blue" });
+        }
+          
       }
 
       } catch (err) {
--- front/src/components/ChatAll.vue
+++ front/src/components/ChatAll.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
   import { reactive } from 'vue';
-  import { io, Socket} from 'socket.io-client';
+  import { io} from 'socket.io-client';
   import { ref } from 'vue';
 
 
@@ -8,7 +8,7 @@
   const apiKey = import.meta.env.VITE_API_KEY;
   
   const state = reactive({
-    messages: [] as { pseudo: string; content: string } [], 
+    messages: [] as { pseudo: string; content: string; class:string } [], 
     newMessage: ''
   });
   
@@ -20,19 +20,21 @@
 }); 
 
   
+  socket.emit('joinRoom',{user:localStorage.getItem('token'),id:1})
 
   const sendMessage = () => {
     if (state.newMessage.trim() !== '') {
       const data={
         pseudo:localStorage.getItem('token'),
-        content:state.newMessage
+        content:state.newMessage,
+        roomId:1,
       }
       socket.emit('message', data);
       state.newMessage = ''; 
     }
   };
 
-  socket.on('messageResponse',(message : {pseudo:string,content:string}) =>{
+  socket.on('messageResponse',(message : {pseudo:string,content:string,class:string}) =>{
       state.messages.push(message)
   })
   
@@ -46,7 +48,7 @@
     <h1>Chat Application</h1>
     <ul>
       <li v-for="(message, index) in state.messages" :key="index">
-          <p><strong>{{ message.pseudo }}</strong> {{ message.content }}</p>
+          <p><strong :class="message.class">{{ message.pseudo }}</strong>{{ message.content }}</p>
       </li>
     </ul>
     <form @submit.prevent="sendMessage">
@@ -59,5 +61,13 @@
 
 <style scoped lang="scss">
 
+  main{
+    .red{
+      color:red;
+    }
+    .blue{
+      color:blue;
+    }
+}
 
 </style>
