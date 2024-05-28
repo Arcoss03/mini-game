557d7051471d5155c13792e9ab9a191ee888b3dd
matteobelin <matteobelin85@gmail.com>
2024-05-28 21:27:52 +0200

    feat: Chat ajout dans la bdd des messages + quand rejoint room affiche les 20 derniers messages

--- api/src/chat.ts
+++ api/src/chat.ts
@@ -13,7 +13,7 @@ const setupSocket = (fastify: FastifyInstance) => {
       credentials: true
     }
   });
-
+  
   io.on('connection', (socket) => {
 
     socket.on('joinRoom', async function(room) {
@@ -21,8 +21,22 @@ const setupSocket = (fastify: FastifyInstance) => {
         const decoded = await fastify.jwt.verify(token);
         const { id } = decoded as { id: string }; 
         const [rows]: any = await fastify.db.query('SELECT pseudo FROM users JOIN chat_participant ON chat_participant.user_id = users.id WHERE users.id = ? AND chat_participant.chat_room_id = ?', [id,room.id]);
+        const pseudo=rows[0].pseudo;
         if(rows.length===1){
           socket.join(room.id);
+          const [rows]: any = await fastify.db.query('SELECT users.pseudo, message.content FROM message JOIN users ON message.sender_id = users.id WHERE  message.chat_room_id = ? ORDER BY message.id DESC LIMIT 20', [room.id]);
+          if(rows.length>=1){
+            rows.reverse();
+            console.log(rows)
+            rows.forEach((row :any) => {
+              if(row.pseudo===pseudo){
+                socket.emit('messageResponse', { pseudo: row.pseudo, content: row.content,class:"blue" });
+              }else{
+                socket.emit('messageResponse', { pseudo: row.pseudo, content: row.content,class:"red" });
+              }
+            });
+            
+          }
         }
   });
 
@@ -39,7 +53,15 @@ const setupSocket = (fastify: FastifyInstance) => {
           if(rows.length===1){
             socket.to(msg.roomId).emit('messageResponse', { pseudo: rows[0].pseudo, content: msg.content,class:"red" });
             socket.emit('messageResponse', { pseudo: rows[0].pseudo, content: msg.content,class:"blue" });
+            await fastify.db.query(
+              'INSERT INTO  message (content, sender_id, chat_room_id) VALUES (?, ?, ?)', 
+              [msg.content, id, msg.roomId]
+          );
         }
+
+        socket.on("leaveRoom", (roomId) => {
+          socket.leave(roomId);
+        });
           
       }
 
