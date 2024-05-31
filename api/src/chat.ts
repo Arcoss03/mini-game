834d30a14209c71052bb50a656a4b3b9a5497ba5
matteobelin <matteobelin85@gmail.com>
2024-05-31 19:13:06 +0200

    fix: Chat everybody can see chat all / redirection to /login

--- api/src/chat.ts
+++ api/src/chat.ts
@@ -15,88 +15,96 @@ const setupSocket = (fastify: FastifyInstance) => {
   });
 
   const colors = [
-    "#FF5733",
-    "#33FF57",
-    "#3357FF",
-    "#FF33A6",
-    "#33FFF5",
-    "#F533FF",
-    "#57FF33",
-    "#5733FF",
-    "#FF3357",
-    "#33FFB2",
-    "#FFA633",
-    "#33A6FF",
-    "#FF5733",
-    "#33FFA6",
-    "#A633FF"
-];
+    "#FF0000", "#FFA500", "#FFFF00", "#00FF00", "#00FFFF",
+    "#0000FF", "#8A2BE2", "#FF1493", "#FF4500", "#FF6347",
+    "#FF69B4", "#FFD700", "#7FFF00", "#00FF7F", "#20B2AA",
+    "#9400D3", "#FF00FF", "#00BFFF", "#FF00FF", "#FF5733",
+    "#33FF57", "#3357FF", "#FF33A6", "#33FFF5", "#F533FF",
+    "#57FF33", "#5733FF", "#FF3357", "#33FFB2", "#FFA633"
+  ];
   
-function generateColor(pseudo:string){
-  let asciiSum = 0;
+  
 
-  for (let i = 0; i < pseudo.length; i++) {
-    if(i%2==0){
-      asciiSum += pseudo.charCodeAt(i);
-    }else{
-      asciiSum -= pseudo.charCodeAt(i);
+  function generateColor(pseudo:string){
+    let asciiSum = 0;
+    for (let i = 0; i < pseudo.length; i++) {
+        asciiSum += pseudo.charCodeAt(i);
     }
-    return (asciiSum>0)?colors[asciiSum%colors.length]:colors[(asciiSum%colors.length)*-1]
-}
-}
+      return (asciiSum>0)?colors[asciiSum%colors.length]:colors[(asciiSum%colors.length)*-1]
+  }
 
 
   io.on('connection', (socket) => {
-
-    socket.on('joinRoom', async function(room) {
-      try{
-        const token = room.user;
-        const decoded = await fastify.jwt.verify(token);
-        const { id } = decoded as { id: string }; 
-        const [rows]: any = await fastify.db.query('SELECT pseudo FROM users JOIN chat_participant ON chat_participant.user_id = users.id WHERE users.id = ? AND chat_participant.chat_room_id = ?', [id,room.id]);
-        if(rows.length===1){
-          socket.join(room.id);
-          const [rows]: any = await fastify.db.query('SELECT users.pseudo, message.content FROM message JOIN users ON message.sender_id = users.id WHERE  message.chat_room_id = ? ORDER BY message.id DESC LIMIT 20', [room.id]);
-          if(rows.length>=1){
-            rows.reverse();
-            rows.forEach((row :any) => {
-              socket.emit('messageResponse', { pseudo: row.pseudo, content: row.content,color:generateColor(row.pseudo) });
-            });
-            
-          }
-        }
-      }catch (err) {
-        socket.emit('error', { error: 'Invalid token' });
-      }
-        
-  });
-
-
-    socket.on('message', async (msg) => {
+    socket.on('joinRoom', async (room) => {
       try {
-        const token = msg.pseudo;
-        const decoded = await fastify.jwt.verify(token);
-        const { id } = decoded as { id: string }; 
-        const [rows]: any = await fastify.db.query('SELECT pseudo FROM users WHERE id = ?', [id]);
-
-        if (rows.length >= 1) {
-          const [rows]: any = await fastify.db.query('SELECT pseudo FROM users JOIN chat_participant ON chat_participant.user_id = users.id WHERE users.id = ? AND chat_participant.chat_room_id = ?', [id,msg.roomId]);
-          if(rows.length===1){
-            socket.to(msg.roomId).emit('messageResponse', { pseudo: rows[0].pseudo, content: msg.content,color:generateColor(rows.pseudo) });
-            socket.emit('messageResponse', { pseudo: rows[0].pseudo, content: msg.content,color:generateColor(rows.pseudo) });
-            await fastify.db.query(
-              'INSERT INTO  message (content, sender_id, chat_room_id) VALUES (?, ?, ?)', 
-              [msg.content, id, msg.roomId]
+        if(room==1){
+          const token = room.user;
+          const decoded = await fastify.jwt.verify(token);
+          const { id } = decoded as { id: string };
+          const [rows]: any = await fastify.db.query(
+            'SELECT pseudo FROM users JOIN chat_participant ON chat_participant.user_id = users.id WHERE users.id = ? AND chat_participant.chat_room_id = ?',
+            [id, room.id]
           );
-        } 
-      }
-      socket.on("leaveRoom", (roomId) => {
-        socket.leave(roomId);
-      });
+  
+          if (rows.length === 1) {
+            socket.join(room.id);}
+        }else{
+          socket.join(room.id);}
+          const [messages]: any = await fastify.db.query(
+            'SELECT users.pseudo, message.content FROM message JOIN users ON message.sender_id = users.id WHERE message.chat_room_id = ? ORDER BY message.id DESC LIMIT 20',
+            [room.id]
+          );
 
+          if (messages.length >= 1) {
+            messages.reverse();
+            messages.forEach((message: any) => {
+              socket.emit('messageResponse', { pseudo: message.pseudo, content: message.content, color: generateColor(message.pseudo) });
+            });
+          }
+        
       } catch (err) {
         socket.emit('error', { error: 'Invalid token' });
       }
+    });
+
+    socket.on('message', async (msg) => {
+      try {
+        // Vérification de la validité du token
+        const token = msg.pseudo;
+        const decoded = await fastify.jwt.verify(token);
+        const { id } = decoded as { id: string };
+      
+        // Récupération du pseudo de l'utilisateur à partir de la base de données
+        const [userRows]: any = await fastify.db.query('SELECT pseudo FROM users WHERE id = ?', [id]);
+        const pseudo = userRows[0].pseudo;
+      
+        // Vérification de la présence de l'utilisateur dans la salle de chat spécifiée
+        const [participantRows]: any = await fastify.db.query(
+          'SELECT pseudo FROM users JOIN chat_participant ON chat_participant.user_id = users.id WHERE users.id = ? AND chat_participant.chat_room_id = ?',
+          [id, msg.roomId]
+        );
+        if (participantRows.length === 1) {
+          const color = generateColor(pseudo);
+          socket.to(msg.roomId).emit('messageResponse', { pseudo, content: msg.content, color });
+          socket.emit('messageResponse', { pseudo, content: msg.content, color });
+          await fastify.db.query('INSERT INTO message (content, sender_id, chat_room_id) VALUES (?, ?, ?)', [msg.content, id, msg.roomId]);
+        } else {
+          socket.emit('error', { error: 'Player not in this room' });
+        }
+      } catch (err) {
+        socket.emit('error', { error: 'Invalid token' });
+      }
+    });
+
+
+    // Adding handler to leave all rooms
+    socket.on("leaveRoom", () => {
+      const rooms = Array.from(socket.rooms);
+      rooms.forEach(room => {
+        if (room !== socket.id) {
+          socket.leave(room);
+        }
+      });
     });
   });
 
