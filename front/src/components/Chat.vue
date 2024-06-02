8ee86981bce8b572d2762727c53190b79bfd4b5a
matteobelin <matteobelin85@gmail.com>
2024-06-02 17:38:08 +0200

    fix:Chat css style

--- front/src/components/Chat.vue
+++ front/src/components/Chat.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-import { reactive, ref, onUpdated, watch } from 'vue';
+import { reactive, ref, onUpdated } from 'vue';
 import socketClient from '../helpers/chatHelper';
 
 const props = defineProps({
     roomName: String,
     roomId: Number,
-  });
+});
 
 let room = props.roomId;
 const state = reactive({
@@ -14,8 +14,10 @@ const state = reactive({
   taille: 0,
 });
 
+const MAX_CHARACTERS = 244;
 const messageList = ref<HTMLElement | null>(null); 
 const chatHeight = ref('calc(100vh - 10rem)'); 
+const textareaRef = ref<HTMLTextAreaElement | null>(null);
 
 const scroll = ref(true);
 
@@ -37,11 +39,12 @@ socketClient.quitRoom();
 socketClient.joinRoom(localStorage.getItem('token') as string, room as number);
 
 const sendMessage = () => {
-  const content = state.textArea;
+  const content = state.textArea.slice(0, MAX_CHARACTERS);
   socketClient.message(localStorage.getItem('token') as string, content, room as number);
   state.textArea = '';
   state.taille = 0;
   scroll.value = true;
+  resetTextarea();
 };
 
 socketClient.messageResponse(state.messages);
@@ -54,11 +57,18 @@ const onInput = (event: Event) => {
 
 const resizeTextarea = (textarea: HTMLTextAreaElement) => {
   textarea.style.height = 'auto'; // Réinitialiser la hauteur
-  if (textarea.scrollHeight >48) {
+  if (textarea.scrollHeight > 48) {
     textarea.style.height = `${textarea.scrollHeight + 16}px`; // Ajuster à la hauteur du contenu
     chatHeight.value = `calc(100vh - ${textarea.scrollHeight + 32}px - 5rem)`; // 32px is the padding and margin combined
   } else {
     chatHeight.value = 'calc(100vh - 10rem)'; 
+  }
+};
+
+const resetTextarea = () => {
+  chatHeight.value = 'calc(100vh - 10rem)';
+  if (textareaRef.value) {
+    textareaRef.value.style.height = 'auto'; // Réinitialiser la hauteur du textarea
   }
 };
 
@@ -69,36 +79,39 @@ onUpdated(() => {
 });
 </script>
 
+
 <template>
-<main style="height: 100%;">
-  <h1>{{props.roomName}}</h1>
-  <div class="barre"></div>
-  <div class="chat">
-    <ul :style="{ height: chatHeight }" ref="messageList" @scroll="handleScroll">
-      <li v-for="(message, index) in state.messages" :key="index">
-        <p>
-          <strong class="pseudo" :style="{ color: message.color }">{{ message.pseudo }} :</strong>
-          {{ message.content }}
-        </p>
-      </li>
-    </ul>
-      <div class="textarea-container">
-        <textarea
-            v-model="state.textArea"
-            @input="onInput"
-            type="text"
-            placeholder="Saisissez votre message..."
-            @keydown.enter.prevent="sendMessage">
-          </textarea>
-      </div>
-  </div>
-</main>
-</template>
+  <main style="height: 100%;">
+    <h1>{{props.roomName}}</h1>
+    <div class="barre"></div>
+    <div class="chat">
+      <ul :style="{ height: chatHeight }" ref="messageList" @scroll="handleScroll">
+        <li v-for="(message, index) in state.messages" :key="index" class="message-item">
+          <strong class="pseudo" :style="{ color: message.color }">{{ message.pseudo }}:</strong>
+          <span class="content">{{ message.content }}</span>
+        </li>
+      </ul>
+        <div class="textarea-container">
+          <textarea
+              v-model="state.textArea"
+              ref="textareaRef"
+              @input="onInput"
+              :maxlength="MAX_CHARACTERS"
+              type="text"
+              placeholder="Saisissez votre message..."
+              @keydown.enter.prevent="sendMessage">
+            </textarea>
+        </div>
+    </div>
+  </main>
+  </template>
+  
+  
 
 <style scoped lang="scss">
 main {
   background-color: #211D2A;
-  .barre{
+  .barre {
     margin-left: 2rem;
     border-radius: 2rem;
     height: 0.15rem;
@@ -111,52 +124,51 @@ main {
   ul {
     list-style-type: none;
   }
-    .textarea-container {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      border-radius: 12px;
-      background: white;
-      width: 80%;
-      margin: 1rem 0 0 1rem;
-      padding: 0 1rem 0 1rem;
-    }
-    textarea {
-      width: 100%;
-      resize: none;
-      overflow: hidden;
-      font-family: Arial, Helvetica, sans-serif;
-      background: none;
-      border: none;
-      padding: 0;
-      line-height: 1rem;
-      padding-top: 1rem;
+  .textarea-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 12px;
+    background: white;
+    width: 80%;
+    margin: 1rem 0 0 1rem;
+    padding: 0 1rem;
+  }
+  textarea {
+    width: 100%;
+    resize: none;
+    overflow: hidden;
+    font-family: Arial, Helvetica, sans-serif;
+    background: none;
+    border: none;
+    padding: 0;
+    line-height: 1rem;
+    padding-top: 1rem;
+    font-size: 1rem;
+    &::placeholder {
       font-size: 1rem;
-      &::placeholder {
-        font-size: 1rem;
-      }
-      &::-webkit-scrollbar {
-        width: 6px;
-        background-color: transparent;
-      }
-      &::-webkit-scrollbar-track {
-        background: transparent;
-        border-radius: 5px;
-      }
-      &::-webkit-scrollbar-thumb {
-        background: #888;
-        border-radius: 6px;
-      }
-      &::-webkit-scrollbar-thumb:hover {
-        background: #555;
-      }
-      &:focus,
-      &:active {
-        border: none;
-        outline: none;
-      }
     }
-  
+    &::-webkit-scrollbar {
+      width: 6px;
+      background-color: transparent;
+    }
+    &::-webkit-scrollbar-track {
+      background: transparent;
+      border-radius: 5px;
+    }
+    &::-webkit-scrollbar-thumb {
+      background: #888;
+      border-radius: 6px;
+    }
+    &::-webkit-scrollbar-thumb:hover {
+      background: #555;
+    }
+    &:focus,
+    &:active {
+      border: none;
+      outline: none;
+    }
+  }
   .pseudo {
     font-weight: 490;
   }
@@ -184,8 +196,22 @@ main {
         border: none;
         outline: none;
       }
+      li {
+        word-break: break-word;
+        color: #fff;
+        .message {
+          display: flex;
+          flex-wrap: wrap;
+        }
+        .pseudo {
+          margin-right: 0.5rem;
+        }
+        .content {
+          flex: 1;
+          word-break: break-word;
+        }
+      }
     }
   }
 }
 </style>
-
