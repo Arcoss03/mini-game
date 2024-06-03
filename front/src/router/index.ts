2d9a051a83d23d8d0f9543f33343e6303b02e47a
Cam- <veillardcamille01@gmail.com>
2024-06-03 18:32:02 +0200

    Create Homepage

--- front/src/router/index.ts
+++ front/src/router/index.ts
@@ -11,7 +11,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: () => import('../views/HomeView.vue')
     },
     {
       path: '/signup',
