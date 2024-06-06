a677eb4db0c6eec0b2d85021ad5b91d3ec105892
Cam- <veillardcamille01@gmail.com>
2024-06-06 15:44:23 +0200

    Add HomePage + Component NavBar

---
+++ front/src/components/Home.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+
+import { onMounted, ref, type Ref} from 'vue';
+import { useUtilsStore } from '@/stores/utilsStore';
+import NavBar from './NavBar.vue';
+
+</script>
+
+
+<template>
+    <main class="home">
+        <NavBar />
+    </main>
+</template>
+
+<style scoped lang="scss">
+main {
+    min-height: 100vh;
+    background: #211D2A;
+    border: solid #17141D;
+    display: flex;
+    flex-direction: column;
+    justify-content: start;
+    align-items: center;
+
+    
+}
+</style>
