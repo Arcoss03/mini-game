bb41da24ab5bc67210a5ac340dd5fddaa465e41c
Arthur FRIN <frin.arthur@gmail.com>
2024-06-20 09:42:27 +0200

    fix: remove console.log

--- front/src/stores/userStore.ts
+++ front/src/stores/userStore.ts
@@ -11,7 +11,6 @@ export const useUserStore = defineStore('userStore', () => {
     // Function to check if the user is logged in using the token (is used at the refresh of the page to keep the user logged in)
     const tokenLogin = async () => {
         const token = localStorage.getItem('token') as string;
-        console.log('isCall');
         if(token) {
             const response = await apiHelper.kyPost('auth/token', {}, token);
             if(response.success) {
