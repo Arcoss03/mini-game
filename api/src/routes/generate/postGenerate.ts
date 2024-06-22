7f322723f43d64f0f50219fc3d74f7cc7dc47def
Arthur FRIN <frin.arthur@gmail.com>
2024-06-22 20:22:39 +0200

    feat: add ai generaion for imgs in tpf creation + rantom style choice

--- api/src/routes/generate/postGenerate.ts
+++ api/src/routes/generate/postGenerate.ts
@@ -1,14 +1,5 @@
 import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
-import axios from 'axios';
-import fs from 'fs';
-import path from 'path';
-import { v4 as uuidv4 } from 'uuid';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-const API_KEY = process.env.DALLE_API_KEY;
-const API_URL = 'https://api.openai.com/v1/images/generations';
+import { generateAndStoreImage } from '../../utils/generateImg.utils';
 
 interface GenerateImageRequestBody {
   prompt: string;
@@ -19,53 +10,15 @@ interface GenerateImageRequest extends FastifyRequest {
 }
 
 const generateImageRoute = async (fastify: FastifyInstance) => {
-  console.log('generateImageRoute', API_KEY, API_URL);
   fastify.post<{ Body: GenerateImageRequestBody }>('/', async (request, reply) => {
     const { prompt } = request.body;
 
     try {
-      const response = await axios.post(API_URL, {
-        prompt,
-        n: 1,
-        size: '256x256',
-      }, {
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${API_KEY}`,
-        },
-      });
-
-      // Log la réponse complète de l'API DALL-E pour débogage
-      fastify.log.info({ dalleResponseData: response.data });
-
-      // Vérifier si l'URL de l'image existe
-      if (!response.data.data || !response.data.data[0] || !response.data.data[0].url) {
-        throw new Error('Invalid response from DALL-E API');
-      }
-
-      // Obtenir l'URL de l'image générée
-      const imageUrl = response.data.data[0].url;
-
-      // Télécharger l'image à partir de l'URL fournie par DALL-E
-      const imageResponse = await axios.get(imageUrl, {
-        responseType: 'arraybuffer',
-      });
-
-      const imageBuffer = Buffer.from(imageResponse.data, 'binary');
-      const uploadsDir = path.join(process.cwd(), 'uploads');
-      const imagePath = path.join(uploadsDir, `${uuidv4()}.png`);
-
-      // Assurez-vous que le répertoire d'uploads existe
-      if (!fs.existsSync(uploadsDir)) {
-        fs.mkdirSync(uploadsDir, { recursive: true });
-      }
-
-      fs.writeFileSync(imagePath, imageBuffer);
-
+      const imagePath = await generateAndStoreImage(prompt);
       reply.send({ success: true, path: imagePath });
     } catch (error) {
       fastify.log.error(error);
-      reply.status(500).send({ error: 'Failed to generate image'});
+      reply.status(500).send({ error: 'Failed to generate image' });
     }
   });
 };
