1ea71322b4f2daa7f1a18848bb8af48466af377c
matteobelin <matteobelin85@gmail.com>
2024-07-04 15:21:14 +0200

    fix: Fix define props

--- front/src/views/GuessMyPromptView.vue
+++ front/src/views/GuessMyPromptView.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
+import { defineProps } from 'vue';
 import gmp from "../components/GuessMyPrompt.vue";
+const props = defineProps<{ gmpId: string }>();
+
 </script>
 <template>
-  <gmp/>
+  <gmp :gmpId="props.gmpId"/>
 </template>
