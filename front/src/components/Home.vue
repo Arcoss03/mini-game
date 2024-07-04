66fc8d604fa59436478a4e81823ffdd6564420dd
Arthur FRIN <frin.arthur@gmail.com>
2024-07-04 12:12:46 +0200

    feat: home page css

--- front/src/components/Home.vue
+++ front/src/components/Home.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 
-import { onMounted, ref, type Ref} from 'vue';
+import { onMounted, ref, type Ref } from 'vue';
 import { useUtilsStore } from '@/stores/utilsStore';
 import { useRouter } from 'vue-router';
 import Chat from './Chat.vue';
@@ -9,7 +9,7 @@ let searchTerm = ref('');
 let router = useRouter();
 
 const submitForm = () => {
-  router.push(`/profil/${searchTerm.value}`);
+    router.push(`/profil/${searchTerm.value}`);
 };
 
 </script>
@@ -17,52 +17,55 @@ const submitForm = () => {
 
 <template>
     <main class="home">
-        <form  @submit.prevent="submitForm">
-            <div class="search">
-                <input type="text" name="test" v-model="searchTerm" placeholder="recherche">
-                <button type="submit"><img src="../assets/search.svg" alt=""></button>
-            </div>
-        </form>
+        <div class="feed">
+            <form @submit.prevent="submitForm">
+                <div class="search">
+                    <input type="text" name="test" v-model="searchTerm" placeholder="recherche">
+                    <button type="submit">
+                        <img src="../assets/search.svg" alt="">
+                    </button>
+                </div>
+            </form>
+            <div class="feed-container">
 
-        <div class="chatbox">
-            <chat :roomId="1" roomName="" maxHeight="100rem"  class="chat" />
+            </div>
+
+        </div>
+        <div class="chat-container">
+            <chat :roomId="1" roomName="" maxHeight="100rem" class="chat" />
         </div>
     </main>
 </template>
 
 <style scoped lang="scss">
-main {
+.home {
     min-height: 100vh;
     background: var(--bg-color);
     border: solid var(--bg-color-elements);
     display: flex;
-    flex-direction: column;
-    justify-content: start;
-    align-items: center;
     width: 100%;
+    overflow: hidden;
 
-    .search {
-        display: none;
-    }
-    .chatbox {
-        display : none;
-    }
+    .feed {
+        flex: 1;
+        margin: 2rem;
 
-    @media (min-width: 769px)  {
         .search {
             display: flex;
-            //position temporaire 
-            position: fixed;
-            top: 1%;
-            right: 0;
+            
 
             input {
+                padding-left: 1rem;
                 width: 250px;
                 height: 40px;
                 border: 2px solid var(--color-text);
                 border-radius: 8px;
             }
+
             button {
+                display: flex;
+                justify-content: center;
+                align-items: center;
                 width: 40px;
                 height: 40px;
                 border: solid #fff;
@@ -70,13 +73,27 @@ main {
                 color: var(--color-text);
             }
         }
-        .chatbox {
-            display: flex;
-            width: 500px;
+    }
+
+    .chat-container {
+        display: none;
+    }
+
+    @media (min-width: 1024px) {
+        .feed {
+            flex: 1.4;
+        }
+
+        .chat-container {
+            display: block;
+            flex: 0.6;
+            width: 100%;
+            height: 100vh;
+            border-left: solid 10px var(--bg-color-elements);
         }
     }
 
 
-    
+
 }
 </style>
