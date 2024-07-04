d75ec55dc3103319be98f9a21bfebd6d0cfc515b
Cam- <veillardcamille01@gmail.com>
2024-07-04 15:16:56 +0200

    Adding the logout button in the settings page

--- front/src/components/Settings.vue
+++ front/src/components/Settings.vue
@@ -2,11 +2,20 @@
 
 import { useUtilsStore } from '@/stores/utilsStore';
 import { useUserStore } from '@/stores/userStore';
+import { onMounted, ref, type Ref } from 'vue';
+import router from '@/router';
+import { on } from 'events';
 
 const profil_picture = useUserStore().currentUser?.profil_picture;
 const name = useUserStore().currentUser?.pseudo;
+const userStore = useUserStore();
+const isLogedIn: Ref<boolean> = ref(false);
 
 const utilsStore = useUtilsStore();
+
+onMounted(() => {
+    isLogedIn.value = userStore.isLogedIn;
+});
 
 const onThemeChange = (event: Event) => {
     const target = (event.target as HTMLSelectElement); // Utilisation de 'as' pour le cast
@@ -15,25 +24,37 @@ const onThemeChange = (event: Event) => {
 
     console.log(selectedTheme);
 };
+const logout = () => {
+    localStorage.removeItem('token');
+    userStore.currentUser = undefined;
+    router.push('/login');
+
+};
 
 </script>
 
 <template>
     <main class="settings">
         <h1>Paramètres</h1>
+        
         <RouterLink v-if="profil_picture !== ''" class="profil" id="log" to="/profil">
             <img class="pp" :src="profil_picture" alt="">
             <div>{{ name }}</div>
         </RouterLink>
+
         <div class="content">
-            <li>
-                <label for="theme">Theme</label>
-                <select name="theme" class="theme" @change="onThemeChange" >
-                    <option value="dark">Dark</option>
-                    <option value="light">Light</option>
-                </select>
+            <label for="theme">Theme</label>
+            <select name="theme" class="theme" @change="onThemeChange">
+                <option value="dark">Dark</option>
+                <option value="light">Light</option>
+            </select>
+
+            <div v-if="isLogedIn">
                 <a href="">Mon score CO2</a>
-            </li>
+    
+                <button @click="logout" class="logout">Se deconnecter</button>
+            </div>
+
         </div>
     </main>
 </template>
@@ -61,13 +82,14 @@ main {
         display: flex;
         align-items: center;
         margin-top: 1rem;
-        gap:1rem;
-        
+        gap: 1rem;
+
         img {
             width: 50px;
             height: 50px;
             border-radius: 999px;
         }
+
         div {
             font-size: 2rem;
         }
@@ -101,6 +123,7 @@ main {
         display: flex;
         flex-direction: column;
         justify-content: start;
+        color: var(--color-text);
 
         .profil {
             display: none;
@@ -111,6 +134,18 @@ main {
             display: flex;
             flex-direction: column;
         }
+        .content {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            margin-top: 20px;
+            color: var(--color-text);
+            .logout {
+                display: flex;
+                color: var(--color-text);
+            }
+        }
+
     }
 }
 </style>
